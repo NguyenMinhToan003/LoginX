049b6f5c4abc5d6ae547344dd8de3b5ca3fc317f
Toan <nguyentoan04.0003@email.com>
2025-03-01 09:36:09 +0700

    fix port

--- src/passport/twitterStrategy.js
+++ src/passport/twitterStrategy.js
@@ -5,7 +5,7 @@ export const twitterStrategy = () => {
   passport.use(new TwitterStrategy({
     consumerKey: process.env.TWITTER_CONSUMER_KEY,
     consumerSecret: process.env.TWITTER_CONSUMER_SECRET,
-    callbackURL: 'http://localhost:8123/api/auth/twitter/callback',
+    callbackURL: `${process.env.PORT}/api/auth/twitter/callback`,
     scope: ['user:email'],
     propmt: 'consert'
   },
