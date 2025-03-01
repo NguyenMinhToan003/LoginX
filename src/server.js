ec5ba31b31b117ec2c7b464ac4844319edaed8c8
Toan <nguyentoan04.0003@email.com>
2025-03-01 10:38:56 +0700

    fix port local test

--- src/server.js
+++ src/server.js
@@ -17,7 +17,7 @@ const START_SERVER = () => {
   const app = express()
   app.use(cors(
     {
-      origin: 'http://192.168.1.165:5173/',
+      origin: 'http://192.168.1.165:5173',
       credentials: true
     }
   ))
