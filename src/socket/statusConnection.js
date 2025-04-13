b7c7278e56c24ed3724c2e694389219f290885b5
Toan <117341351+NguyenMinhToan003@users.noreply.github.com>
2025-04-13 19:40:06 +0700

    Update statusConnection.js

--- src/socket/statusConnection.js
+++ src/socket/statusConnection.js
@@ -1,24 +1,32 @@
-let listUsers = [];
+let listUsers = []
 
 export const StatusConnection = (socket, io) => {
-  socket.on("disconnect", () => {
-    const index = listUsers.findIndex((user) => user.socketId === socket.id);
-    if (index !== -1) {
-      listUsers.splice(index, 1); 
-    }
-    io.emit("getListUsers", listUsers);
-  });
-  socket.emit("getListUsers", listUsers);
 
-  socket.on("addNewUser", (data) => {
+  socket.on('join-list-rooms', (rooms) => {
+    rooms.forEach((room) => {
+      socket.join(room)
+    })
+  })
+
+
+  socket.on('disconnect', () => {
+    const index = listUsers.findIndex((user) => user.socketId === socket.id)
+    if (index !== -1) {
+      listUsers.splice(index, 1) 
+    }
+    io.emit('getListUsers', listUsers)
+  })
+  socket.emit('getListUsers', listUsers)
+
+  socket.on('addNewUser', (data) => {
     if (listUsers.findIndex((user) => user.userId === data._id) === -1) {
       listUsers.push({
         userId: data._id,
         socketId: socket.id,
         name: data.name,
         picture: data.picture,
-      });
+      })
     }
-    io.emit("getListUsers", listUsers);
-  });
-};
+    io.emit('getListUsers', listUsers)
+  })
+}
